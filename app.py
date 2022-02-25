c187b934a67205b18dfdf68e69262dd02a8839d8
ljy <1063625147@qq.com>
2022-02-25 17:49:51 +0800

    2/25 update

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+from test import getdata
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/test')
+def showdata():
+    data=getdata()
+    return data
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
